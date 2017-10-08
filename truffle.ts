2d093ad1696b1fec420a245c9611e24b77e4214e
Marcin Biernat <mb@marcinbiernat.pl>
2017-10-08 18:16:19 +0200

    Codeship integration

--- truffle.ts
+++ truffle.ts
@@ -1,7 +1,13 @@
+import * as yargs from 'yargs';
+
+const argv = yargs.option('rpc-host', {
+  default: 'localhost'
+}).argv;
+
 export = {
   networks: {
     development: {
-      host: 'localhost',
+      host: argv.rpcHost,
       network_id: '*',
       port: 8545
     }
