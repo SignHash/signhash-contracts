8b9841f4fb5345f17e67b7d5e0dd257c5f61292c
Jakub Stefanski <code@jakubstefanski.com>
2017-12-07 19:36:25 +0100

    Modularize RecoverableMultiSigTests

--- test/MultiSig/recoverablemultisig.test.ts
+++ test/MultiSig/recoverablemultisig.test.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from 'bignumber.js';
 import { assert } from 'chai';
 
 import * as tempo from '@digix/tempo';
@@ -24,17 +25,17 @@ declare const web3: Web3;
 declare const artifacts: SignHashArtifacts;
 declare const contract: ContractContextDefinition;
 
-const waitUntilBlock = tempo(web3).waitUntilBlock;
-
 const RecoverableMultiSigContract = artifacts.require(
   './MultiSig/RecoverableMultiSig.sol'
 );
 
-contract('RecoverableMultiSigContract', accounts => {
-  const owners = accounts.slice(1, 3);
-  const newOwners = accounts.slice(6, 9);
-  const nonOwner = accounts[0];
+export class RecoverableMultiSigTestContext {
+  public instance: RecoverableMultiSig;
 
+  public constructor(public accounts: Address[], public owners: Address[]) {}
+}
+
+contract('RecoverableMultiSigContract', accounts => {
   describe('#ctor', () => {
     it('should not allow empty list of owners', async () => {
       await assertThrowsInvalidOpcode(async () => {
@@ -43,78 +44,259 @@ contract('RecoverableMultiSigContract', accounts => {
     });
   });
 
-  context(`When wallet has ${owners.length} owners`, () => {
-    const recoveryConfirmations = 100;
+  const ownerSets = [[accounts[2]], accounts.slice(1, 3), accounts.slice(2, 6)];
+  ownerSets.map(owners => {
+    context(`When wallet has ${owners.length} owners`, () => {
+      const ctx = new RecoverableMultiSigTestContext(accounts, owners);
 
-    let instance: RecoverableMultiSig;
+      beforeEach(async () => {
+        const recoveryConfirmations = 100;
+        ctx.instance = await RecoverableMultiSigContract.new(
+          owners,
+          recoveryConfirmations
+        );
+      });
 
+      describe('#ctor', () => {
+        it('should set owners', async () => {
+          assert.deepEqual(await ctx.instance.listOwners(), owners);
+        });
+      });
+
+      describe('#startRecovery', () => testStartRecovery(ctx));
+      describe('#cancelRecovery', () => testCancelRecovery(ctx));
+      describe('#confirmRecovery', () => testConfirmRecovery(ctx));
+    });
+  });
+});
+
+export function testStartRecovery(ctx: RecoverableMultiSigTestContext) {
+  const nonOwner = ctx.accounts[0];
+  const newOwners = ctx.accounts.slice(6, 9);
+
+  // preconditions
+  beforeEach(async () => {
+    assert.deepEqual(ctx.owners, await ctx.instance.listOwners());
+    assert.notInclude(ctx.owners, nonOwner);
+    assert.notDeepEqual(newOwners, ctx.owners);
+  });
+
+  context(`When called by non-owner`, () => {
+    it('should throw invalid opcode', async () => {
+      await assertThrowsInvalidOpcode(async () => {
+        await ctx.instance.startRecovery(newOwners, { from: nonOwner });
+      });
+    });
+  });
+
+  ctx.owners.map((owner, index) => {
+    context(`When called by #${index + 1} owner`, () => {
+      it('should set recoveryHash for a single account', async () => {
+        const singleOwner = [newOwners[0]];
+        await ctx.instance.startRecovery(singleOwner, { from: owner });
+
+        assert.equal(
+          await ctx.instance.recoveryHash(),
+          hashAddresses(singleOwner)
+        );
+      });
+
+      it('should set recoveryHash for multiple accounts', async () => {
+        await ctx.instance.startRecovery(newOwners, { from: owner });
+
+        assert.equal(
+          await ctx.instance.recoveryHash(),
+          hashAddresses(newOwners)
+        );
+      });
+
+      it('should set recoveryBlock to current block', async () => {
+        await ctx.instance.startRecovery(newOwners, { from: owner });
+
+        assertNumberEqual(
+          await ctx.instance.recoveryBlock(),
+          web3.eth.blockNumber
+        );
+      });
+
+      it('should emit RecoveryStarted event', async () => {
+        const trans = await ctx.instance.startRecovery(newOwners, {
+          from: owner
+        });
+
+        const log = findLastLog(trans, 'RecoveryStarted');
+        const event: RecoveryStartedEvent = log.args;
+
+        assert.equal(event.from, owner);
+        assert.deepEqual(event.newOwners, newOwners);
+      });
+    });
+  });
+}
+
+export function testCancelRecovery(ctx: RecoverableMultiSigTestContext) {
+  const nonOwner = ctx.accounts[0];
+  const newOwners = ctx.accounts.slice(6, 9);
+
+  // preconditions
+  beforeEach(async () => {
+    assert.deepEqual(ctx.owners, await ctx.instance.listOwners());
+    assert.notInclude(ctx.owners, nonOwner);
+    assert.notDeepEqual(newOwners, ctx.owners);
+  });
+
+  context('When recovery not started', () => {
+    it('should throw invalid opcode', async () => {
+      await assertThrowsInvalidOpcode(async () => {
+        await ctx.instance.cancelRecovery({ from: ctx.owners[0] });
+      });
+    });
+  });
+
+  context('When recovery started', () => {
     beforeEach(async () => {
-      instance = await RecoverableMultiSigContract.new(
-        owners,
-        recoveryConfirmations
-      );
+      await ctx.instance.startRecovery(newOwners, { from: ctx.owners[0] });
     });
 
-    describe('#ctor', () => {
-      it('should set owners', async () => {
-        assert.deepEqual(await instance.listOwners(), owners);
+    context(`When called by non-owner`, () => {
+      it('should throw invalid opcode', async () => {
+        await assertThrowsInvalidOpcode(async () => {
+          await ctx.instance.cancelRecovery({ from: nonOwner });
+        });
       });
     });
 
-    function hashAddresses(addresses: Address[]): string {
-      const str = addresses.reduce(
-        (acc, owner) => acc.concat(toHex(owner, 64)),
-        ''
-      );
-      return web3.sha3(str, { encoding: 'hex' });
-    }
+    ctx.owners.map((owner, index) => {
+      context(`When called by #${index + 1} owner`, () => {
+        context(`When called by #${index + 1} owner`, () => {
+          it('should reset recoveryHash', async () => {
+            await ctx.instance.cancelRecovery({ from: owner });
 
-    describe('#startRecovery', () => {
+            assert.equal(
+              await ctx.instance.recoveryHash(),
+              `0x${'0'.repeat(64)}`
+            );
+          });
+
+          it('should reset recoveryBlock', async () => {
+            await ctx.instance.cancelRecovery({ from: owner });
+
+            assertNumberEqual(await ctx.instance.recoveryBlock(), 0);
+          });
+
+          it('should emit RecoveryCancelled event', async () => {
+            const trans = await ctx.instance.cancelRecovery({
+              from: owner
+            });
+
+            const log = findLastLog(trans, 'RecoveryCancelled');
+            const event: RecoveryCancelledEvent = log.args;
+
+            assert.equal(event.from, owner);
+          });
+        });
+      });
+    });
+  });
+}
+
+export function testConfirmRecovery(ctx: RecoverableMultiSigTestContext) {
+  const nonOwner = ctx.accounts[0];
+  const newOwners = ctx.accounts.slice(6, 9);
+
+  // preconditions
+  beforeEach(async () => {
+    assert.deepEqual(ctx.owners, await ctx.instance.listOwners());
+    assert.notInclude(ctx.owners, nonOwner);
+    assert.notDeepEqual(newOwners, ctx.owners);
+  });
+
+  context('When recovery not started', () => {
+    it('should throw invalid opcode', async () => {
+      await assertThrowsInvalidOpcode(async () => {
+        await ctx.instance.cancelRecovery({ from: ctx.owners[0] });
+      });
+    });
+  });
+
+  context('When recovery started', () => {
+    const waitUntilBlock = tempo(web3).waitUntilBlock;
+
+    let recoveryPassBlock: BigNumber;
+
+    beforeEach(async () => {
+      await ctx.instance.startRecovery(newOwners, { from: ctx.owners[0] });
+
+      recoveryPassBlock = (await ctx.instance.recoveryBlock()).add(
+        await ctx.instance.recoveryConfirmations()
+      );
+    });
+
+    context('When recovery just started', () => {
+      it('should throw invalid opcode', async () => {
+        await assertThrowsInvalidOpcode(async () => {
+          await ctx.instance.confirmRecovery(newOwners, {
+            from: ctx.owners[0]
+          });
+        });
+      });
+    });
+
+    context('When recovery not passed', () => {
+      it('should throw invalid opcode', async () => {
+        await waitUntilBlock(100, recoveryPassBlock.div(2).toNumber());
+
+        await assertThrowsInvalidOpcode(async () => {
+          await ctx.instance.confirmRecovery(newOwners, {
+            from: ctx.owners[0]
+          });
+        });
+      });
+    });
+
+    context('When recovery passed', () => {
+      beforeEach(async () => {
+        await waitUntilBlock(100, recoveryPassBlock.toNumber());
+      });
+
       context(`When called by non-owner`, () => {
         it('should throw invalid opcode', async () => {
           await assertThrowsInvalidOpcode(async () => {
-            await instance.startRecovery(newOwners, { from: nonOwner });
+            await ctx.instance.confirmRecovery(newOwners, { from: nonOwner });
           });
         });
       });
 
-      owners.map((owner, index) => {
+      ctx.owners.map((owner, index) => {
         context(`When called by #${index + 1} owner`, () => {
-          it('should set recoveryHash for a single account', async () => {
-            const singleOwner = [newOwners[0]];
-            await instance.startRecovery(singleOwner, { from: owner });
+          it('should change owners', async () => {
+            await ctx.instance.confirmRecovery(newOwners, { from: owner });
+
+            assert.deepEqual(await ctx.instance.listOwners(), newOwners);
+          });
+
+          it('should reset recoveryHash', async () => {
+            await ctx.instance.confirmRecovery(newOwners, { from: owner });
 
             assert.equal(
-              await instance.recoveryHash(),
-              hashAddresses(singleOwner)
+              await ctx.instance.recoveryHash(),
+              `0x${'0'.repeat(64)}`
             );
           });
 
-          it('should set recoveryHash for multiple accounts', async () => {
-            await instance.startRecovery(newOwners, { from: owner });
+          it('should reset recoveryBlock', async () => {
+            await ctx.instance.confirmRecovery(newOwners, { from: owner });
 
-            assert.equal(
-              await instance.recoveryHash(),
-              hashAddresses(newOwners)
-            );
+            assertNumberEqual(await ctx.instance.recoveryBlock(), 0);
           });
 
-          it('should set recoveryBlock to current block', async () => {
-            await instance.startRecovery(newOwners, { from: owner });
-
-            assertNumberEqual(
-              await instance.recoveryBlock(),
-              web3.eth.blockNumber
-            );
-          });
-
-          it('should emit RecoveryStarted event', async () => {
-            const trans = await instance.startRecovery(newOwners, {
+          it('should emit RecoveryConfirmed event', async () => {
+            const trans = await ctx.instance.confirmRecovery(newOwners, {
               from: owner
             });
 
-            const log = findLastLog(trans, 'RecoveryStarted');
-            const event: RecoveryStartedEvent = log.args;
+            const log = findLastLog(trans, 'RecoveryConfirmed');
+            const event: RecoveryConfirmedEvent = log.args;
 
             assert.equal(event.from, owner);
             assert.deepEqual(event.newOwners, newOwners);
@@ -122,132 +304,13 @@ contract('RecoverableMultiSigContract', accounts => {
         });
       });
     });
-
-    describe('#cancelRecovery', () => {
-      it('should fail when called when recovery not started', async () => {
-        await assertThrowsInvalidOpcode(async () => {
-          await instance.cancelRecovery({ from: owners[0] });
-        });
-      });
-
-      context('When recovery started', () => {
-        beforeEach(async () => {
-          await instance.startRecovery(newOwners, { from: owners[0] });
-        });
-
-        context(`When called by non-owner`, () => {
-          it('should throw invalid opcode', async () => {
-            await assertThrowsInvalidOpcode(async () => {
-              await instance.cancelRecovery({ from: nonOwner });
-            });
-          });
-        });
-
-        owners.map((owner, index) => {
-          context(`When called by #${index + 1} owner`, () => {
-            it('should reset recoveryHash', async () => {
-              await instance.cancelRecovery({ from: owner });
-
-              assert.equal(
-                await instance.recoveryHash(),
-                `0x${'0'.repeat(64)}`
-              );
-            });
-
-            it('should reset recoveryBlock', async () => {
-              await instance.cancelRecovery({ from: owner });
-
-              assertNumberEqual(await instance.recoveryBlock(), 0);
-            });
-
-            it('should emit RecoveryCancelled event', async () => {
-              const trans = await instance.cancelRecovery({
-                from: owner
-              });
-
-              const log = findLastLog(trans, 'RecoveryCancelled');
-              const event: RecoveryCancelledEvent = log.args;
-
-              assert.equal(event.from, owner);
-            });
-          });
-        });
-      });
-    });
-
-    describe('#confirmRecovery', () => {
-      it('should fail when called when recovery not started', async () => {
-        await assertThrowsInvalidOpcode(async () => {
-          await instance.cancelRecovery({ from: owners[0] });
-        });
-      });
-
-      context('When recovery started', () => {
-        beforeEach(async () => {
-          await instance.startRecovery(newOwners, { from: owners[0] });
-        });
-
-        it('should fail when recovery not passed', async () => {
-          await assertThrowsInvalidOpcode(async () => {
-            await instance.confirmRecovery(newOwners, { from: owners[0] });
-          });
-        });
-
-        context('When recovery passed', () => {
-          beforeEach(async () => {
-            const targetBlock = (await instance.recoveryBlock()).add(
-              await instance.recoveryConfirmations()
-            );
-
-            await waitUntilBlock(100, targetBlock.toNumber());
-          });
-
-          context(`When called by non-owner`, () => {
-            it('should throw invalid opcode', async () => {
-              await assertThrowsInvalidOpcode(async () => {
-                await instance.confirmRecovery(newOwners, { from: nonOwner });
-              });
-            });
-          });
-
-          owners.map((owner, index) => {
-            context(`When called by #${index + 1} owner`, () => {
-              it('should change owners', async () => {
-                await instance.confirmRecovery(newOwners, { from: owner });
-
-                assert.deepEqual(await instance.listOwners(), newOwners);
-              });
-
-              it('should reset recoveryHash', async () => {
-                await instance.confirmRecovery(newOwners, { from: owner });
-
-                assert.equal(
-                  await instance.recoveryHash(),
-                  `0x${'0'.repeat(64)}`
-                );
-              });
-
-              it('should reset recoveryBlock', async () => {
-                await instance.confirmRecovery(newOwners, { from: owner });
-
-                assertNumberEqual(await instance.recoveryBlock(), 0);
-              });
-
-              it('should emit RecoveryConfirmed event', async () => {
-                const trans = await instance.confirmRecovery(newOwners, {
-                  from: owner
-                });
-
-                const log = findLastLog(trans, 'RecoveryConfirmed');
-                const event: RecoveryConfirmedEvent = log.args;
-
-                assert.equal(event.from, owner);
-                assert.deepEqual(event.newOwners, newOwners);
-              });
-            });
-          });
-        });
-      });
-    });
   });
-});
+}
+
+function hashAddresses(addresses: Address[]): string {
+  const str = addresses.reduce(
+    (acc, owner) => acc.concat(toHex(owner, 64)),
+    ''
+  );
+  return web3.sha3(str, { encoding: 'hex' });
+}
