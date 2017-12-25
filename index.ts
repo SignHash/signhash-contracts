98a9d9ff4b24ca66a03a22f31d985e51ff74fbed
Jakub Stefanski <code@jakubstefanski.com>
2017-12-25 23:22:08 +0100

    Update index with new contracts

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 import * as SignHashContract from './build/contracts/SignHash.json';
+import * as SignProofContract from './build/contracts/SignProof.json';
+import * as TipsWalletContract from './build/contracts/TipsWallet.json';
 
 export = {
-  SignHashContract
+  contracts: {
+    SignHash: SignHashContract,
+    SignProof: SignProofContract,
+    TipsWallet: TipsWalletContract
+  }
 };
